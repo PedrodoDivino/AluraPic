6b98a613d4ae87e0344d8ce833969ecad38a7e9b
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2022-10-03 16:02:53 -0300

    ajustes na pagina e adicioao de slit e v-binds

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,72 @@
+
 <template>
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
-    <ul>
-      <li class="lista-fotos" v-for="foto in fotos" :key="foto">
-        <img :src="foto.url" :alt="foto.titulo" :key="foto.id" />
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
-  data() {
-    return {
-      titulo: "Alurapic",
-      fotos: []
-    };
+
+  components: {
+    'meu-painel' : Painel
   },
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic',
+      fotos: []
+    }
+  },
+
   created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
+
+    this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
-      .then(fotos => ((this.fotos = fotos), err => console.log(err)));
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
-};
+}
+
 </script>
+
 <style>
   .corpo {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
-
   }
-  .centralizado{
+
+  .centralizado {
+
     text-align: center;
-
   }
-  .lista-fotos{
+
+  .lista-fotos {
     list-style: none;
   }
-  .lista-fotos{
+
+  .lista-fotos .lista-fotos-item {
+
     display: inline-block;
+  }
+
+  .imagem-responsiva {
+
+    width: 100%;
   }
 </style>
