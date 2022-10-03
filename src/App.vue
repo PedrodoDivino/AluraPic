4def3415203d6e79b2f2102455fea0e4f7c7151f
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2022-10-03 13:47:59 -0300

    Estilizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,45 @@
-<!-- alurapic/src/App.vue -->
-
 <template>
-
-  <div>
-    <h1>{{ titulo }}</h1>
-
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
     <ul>
-
-      <li v-for="foto of fotos" :key="foto.id">
-        <img :src="foto.url" :alt="foto.titulo">
+      <li class="lista-fotos" v-for="foto in fotos" :key="foto">
+        <img :src="foto.url" :alt="foto.titulo" :key="foto.id" />
       </li>
-
     </ul>
-
   </div>
-
 </template>
 
 <script>
-
 export default {
-
   data() {
     return {
-      titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-          titulo: 'Cachorro'
-        },
-        {
-          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
-          titulo: 'Gato'
-        }
-      ]
-
-    }
+      titulo: "Alurapic",
+      fotos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => ((this.fotos = fotos), err => console.log(err)));
   }
-}
+};
 </script>
-
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+
+  }
+  .centralizado{
+    text-align: center;
+
+  }
+  .lista-fotos{
+    list-style: none;
+  }
+  .lista-fotos{
+    display: inline-block;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import VueResource from 'vue-resource';
+
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.use(VueResource);
 new Vue({
   el: '#app',
   render: h => h(App)
